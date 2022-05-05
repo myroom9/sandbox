b52a3c1341cef6af47d7923b0be74de5edca56b4
wonhwiahn <myroom9@naver.com>
2022-05-05 16:54:35 +0900

    [test] test

--- src/main/java/com/whahn/sandbox/domain/salesmanagement/SalesManagementRepositoryCustom.java
+++ src/main/java/com/whahn/sandbox/domain/salesmanagement/SalesManagementRepositoryCustom.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static com.whahn.sandbox.domain.salesmanagement.dto.request.SalesManagementRequestDto.*;
 import static com.whahn.sandbox.domain.salesmanagement.dto.response.SalesManagementResponseDto.*;
 
-public interface SalesManagementRepositoryCustom {
+public interface SalesManagementRepositoryCustom { 
     List<ChannelSalesMonthly> findMonthlyChannelSalesByChannelIdAndYearMonth(ChannelSalesMonthlyRequest condition);
 
     List<TotalSalesMonthly> findMonthlyTotalSalesByYearMonth(TotalSalesMonthlyRequest condition);
